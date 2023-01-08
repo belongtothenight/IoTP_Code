1e4cf441edc4ace0cf1169d3b996e495aacf02cc
belongtothenight <106849935+belongtothenight@users.noreply.github.com>
2023-01-08 22:48:44 +0800

    update

--- src/final/public/dataRetrieve.js
+++ src/final/public/dataRetrieve.js
@@ -2,7 +2,8 @@ export default class DataRetrieve {
     constructor() {
         const data = {
             'src sites': {
-                'TWCWB': 'https://opendata.cwb.gov.tw/dist/opendata-swagger.html#/',
+                'TWCWB_OpenData': 'https://opendata.cwb.gov.tw/dist/opendata-swagger.html#/',
+                'TWCWB_Document': 'https://opendata.cwb.gov.tw/dataset/forecast?page=1',
             },
             'APIs': {
                 'Weather Forecast': {
@@ -10,6 +11,7 @@ export default class DataRetrieve {
                     'link': 'https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization='
                 },
                 'Tide Forecast': {
+                    'updateFrequency': '24', // 24 hours
                     'link': 'https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-A0021-001?Authorization='
                 },
                 'Health Weather Cold Injury': {
@@ -17,8 +19,10 @@ export default class DataRetrieve {
                     'link': 'https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-A0085-003?Authorization='
                 },
                 'Weather Observation': {
+                    'updateFrequency': '1', // 1 hour
                     'link': 'https://opendata.cwb.gov.tw/api/v1/rest/datastore/O-A0001-001?Authorization='
                 },
+                // more can be added...
             },
             'Token': {
                 'User01': 'CWB-8523DB1B-D613-401B-849E-BD0A19FAB9E6',
@@ -26,8 +30,8 @@ export default class DataRetrieve {
         }
     }
 
-    _initAPIData() {
-        console.log('initAPIData');
+    _requestAPI() {
+        console.log('requestAPI');
     }
 
 };
