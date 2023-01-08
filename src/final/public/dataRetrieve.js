f01b7b47201c8b6275b96494ec1cf2ba6a93e89d
belongtothenight <106849935+belongtothenight@users.noreply.github.com>
2023-01-08 22:40:41 +0800

    update

--- src/final/public/dataRetrieve.js
+++ src/final/public/dataRetrieve.js
@@ -1,8 +1,31 @@
 export default class DataRetrieve {
     constructor() {
-        console.log('dataRetrieve');
-        this._initAPIData();
+        const data = {
+            'src sites': {
+                'TWCWB': 'https://opendata.cwb.gov.tw/dist/opendata-swagger.html#/',
+            },
+            'APIs': {
+                'Weather Forecast': {
+                    'updateFrequency': '6', // 6 hours
+                    'link': 'https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization='
+                },
+                'Tide Forecast': {
+                    'link': 'https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-A0021-001?Authorization='
+                },
+                'Health Weather Cold Injury': {
+                    'updateFrequency': '6', // 6 hours
+                    'link': 'https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-A0085-003?Authorization='
+                },
+                'Weather Observation': {
+                    'link': 'https://opendata.cwb.gov.tw/api/v1/rest/datastore/O-A0001-001?Authorization='
+                },
+            },
+            'Token': {
+                'User01': 'CWB-8523DB1B-D613-401B-849E-BD0A19FAB9E6',
+            }
+        }
     }
+
     _initAPIData() {
         console.log('initAPIData');
     }
